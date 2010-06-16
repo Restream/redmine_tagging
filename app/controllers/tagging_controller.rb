24494a80359347b8824e53912d5155f05a7f779c
friflaj <friflaj@ajellito.com>
2010-06-16 14:32:14 +0200

    Sort on closed status

--- app/controllers/tagging_controller.rb
+++ app/controllers/tagging_controller.rb
@@ -6,7 +6,13 @@ class TaggingController < ApplicationController
   def index
     @tags = params[:tags].split
 
-    @issues = Issue.tagged_with(@tags, :on => @project.identifier)
+    @issues = Issue.tagged_with(@tags, :on => @project.identifier).sort {|a, b|
+      if b.closed? == a.closed?
+        b.updated_on <=> a.updated_on
+      else
+        (b.closed? ? 1 : 0) <=> (a.closed? ? 1 : 0)
+      end
+    }
     @wikipages = WikiPage.tagged_with(@tags, :on => @project.identifier)
 
     render :action => "tagged"
