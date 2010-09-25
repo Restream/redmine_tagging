d49eb98fc07f5ad244f102440a8c8da17a80c9ba
friflaj <friflaj@ajellito.com>
2010-09-25 22:55:05 +0200

    Tag autocompletion, better search hits

--- lib/tagging_hooks.rb
+++ lib/tagging_hooks.rb
@@ -28,7 +28,7 @@ module TaggingPlugin
 
         tag_context = issue.project.identifier.gsub('-', '_')
 
-        tags = issue.tag_list_on(tag_context).sort.join(' ')
+        tags = issue.tag_list_on(tag_context).sort.collect{|tag| tag.gsub(/^#/, '')}.join(' ')
 
         return '<p>' + context[:form].text_field(:tags, :value => tags) + '</p>'
       end
@@ -39,7 +39,7 @@ module TaggingPlugin
         issue = context[:issue]
         tags = context[:params]['issue']['tags'].to_s
 
-        tags = tags.split(/[\s,]+/).join(', ')
+        tags = tags.split(/[\s,]+/).collect{|tag| "##{tag}"}.join(', ')
         tag_context = issue.project.identifier.gsub('-', '_')
 
         issue.set_tag_list_on(tag_context, tags)
@@ -71,19 +71,23 @@ module TaggingPlugin
         end
 
         if request.parameters['action'] == 'edit'
-          tags = page.tag_list_on(tag_context).sort.join(' ')
-          tags = "<p
-          id='tagging_wiki_edit_field'><label>#{l(:field_tags)}</label><br /><input id='wikipage_tags' name='wikipage_tags' size='120' type='text' value='#{h(tags)}'/></p>"
+          tags = page.tag_list_on(tag_context).sort.collect{|tag| tag.gsub(/^#/, '')}.join(' ')
+          tags = "<p id='tagging_wiki_edit_block'><label>#{l(:field_tags)}</label><br /><input id='wikipage_tags' name='wikipage_tags' size='120' type='text' value='#{h(tags)}'/></p>"
 
-          # we add marker data at the end of the body because
-          # otherwise the save method doesn't get called. This is
-          # nuts, people.
+          ac = ActsAsTaggableOn::Tag.find(:all,
+              :conditions => ["id in (select tag_id from taggings
+              where taggable_type in ('WikiPage', 'Issue') and context = ?)", tag_context]).collect {|tag| tag.name}
+          ac = ac.collect{|tag| "'#{escape_javascript(tag.gsub(/^#/, ''))}'"}.join(', ')
+
           tags += javascript_include_tag 'jquery-1.4.2.min.js', :plugin => 'redmine_tagging'
+          tags += javascript_include_tag 'tag.js', :plugin => 'redmine_tagging'
+
           tags += <<-generatedscript
             <script type="text/javascript">
               var $j = jQuery.noConflict();
               $j(document).ready(function() {
-                $j('#tagging_wiki_edit_field').insertAfter($j("#content_text").parent().parent());
+                $j('#tagging_wiki_edit_block').insertAfter($j("#content_text").parent().parent());
+                $j('#wikipage_tags').tagSuggest({ tags: [#{ac}] });
               });
             </script>
           generatedscript
@@ -97,11 +101,28 @@ module TaggingPlugin
 
         project = context[:page].wiki.project
 
-        tags = context[:params]['wikipage_tags'].to_s.split(/[\s,]+/).join(', ')
+        tags = context[:params]['wikipage_tags'].to_s.split(/[\s,]+/).collect{|tag| "##{tag}"}.join(', ')
         tag_context = project.identifier.gsub('-', '_')
 
         context[:page].set_tag_list_on(tag_context, tags)
         context[:page].save
+      end
+
+      def view_layouts_base_html_head(context = {})
+        return "
+          <style>
+            span.tagMatches {
+              margin-left: 10px;
+            }
+      
+            span.tagMatches span {
+              padding: 2px;
+              margin-right: 4px;
+              background-color: #0000AB;
+              color: #fff;
+              cursor: pointer;
+            }
+          </style>"
       end
 
     end
