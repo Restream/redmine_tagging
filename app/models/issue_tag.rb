b0386a7db40bad59640c28e308ef0946ec994c53
Stas Kelvich <stanconn@gmail.com>
2011-03-22 15:35:54 +0300

    Added tags for the project report

--- app/models/issue_tag.rb
+++ app/models/issue_tag.rb
@@ -8,6 +8,7 @@ class IssueTag < ActiveRecord::Base
   def title
     tag.gsub(/^#/, '')
   end
+  alias_method :name, :title
 
   def project
     # self.issue.project
@@ -18,4 +19,26 @@ class IssueTag < ActiveRecord::Base
   def before_destroy
     raise ActiveRecord::ReadOnlyRecord
   end
+
+  def self.by_issue_status(project = nil)
+    project_condition = project.nil? ? '' : "and i.project_id=#{project.id}"
+
+    ActiveRecord::Base.connection.select_all(
+      "select
+        s.id as status_id,
+        s.is_closed as closed,
+        t.id as tag,
+        count(i.id) as total
+      from
+        #{Issue.table_name} i, #{IssueStatus.table_name} s,
+        #{ActsAsTaggableOn::Tagging.table_name} ti, #{ActsAsTaggableOn::Tag.table_name} t
+      where
+        i.status_id=s.id
+        and ti.taggable_id=i.id
+        and ti.tag_id=t.id
+        #{project_condition}
+      group
+        by s.id, s.is_closed, t.id")
+  end
+
 end
